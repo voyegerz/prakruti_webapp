dedf26eeaee12ef0017dad7fa97d1d73bece1ebb
voyegerz <nirajkumaruiflow@gmail.com>
2023-09-22 01:45:03 +0530

    Header design complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Sidebar from './Sidebar/Sidebar';
+import Header from './Header/Header';
 
 function App() {
   return (
     <div className="App">
       <Sidebar/>
+      <Header/>
     </div>
   );
 }
--- src/Sidebar/Row/Row.jsx
+++ src/Sidebar/Row/Row.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import './row.css'
-import icon from './icons/bio.png'
+import assessment from './icons/assessment.png'
+import settings from './icons/settings.png'
+import account from './icons/account.png'
+
 
 export default function Row(props) {
+  var icon = assessment;
+  if (props.text === "Assessment"){
+    icon = assessment;
+  }else if(props.text === "Settings"){
+    icon = settings;
+  }else if(props.text === "Account"){
+    icon = account;
+  }
+
   return (
     <div className='row'>
         <img src={icon} className='icon' alt="" />
